555ee8e5cae0cb01274873d07ba0f7136616c73b
alex casadio <alex.casadio92@gmail.com>
2023-04-05 15:22:05 +0200

    added ValidPath attribute to core project

---
+++ Core/DataAnnotations/ValidPath.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class ValidPathAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        var path = value as string;
+        if (string.IsNullOrEmpty(path))
+        {
+            return false;
+        }
+
+        try
+        {
+            Path.GetFullPath(path);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
