10140b35a0a5c3558daadb6b0b1c5332ff26b54b
alex casadio <alex.casadio92@gmail.com>
2023-04-05 19:06:52 +0200

    added file scoped namespace in MonitorConfiguraiton

--- CaptureAgent/Configurations/MonitorConfiguration.cs
+++ CaptureAgent/Configurations/MonitorConfiguration.cs
@@ -1,12 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace CaptureAgent.Configurations
+namespace CaptureAgent.Configurations;
+public class MonitorConfiguration
 {
-    public class MonitorConfiguration
-    {
-        [Range(1, int.MaxValue)]
-        public int Width { get; set; }
-        [Range(1, int.MaxValue)]
-        public int Height { get; set; }
-    }
+    [Range(1, int.MaxValue)]
+    public int Width { get; set; }
+    [Range(1, int.MaxValue)]
+    public int Height { get; set; }
 }
+
