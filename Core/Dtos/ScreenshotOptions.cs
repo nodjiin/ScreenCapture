ff0de272c51d365c9c5a7d5409f8b0189bed7360
alex casadio <alex.casadio92@gmail.com>
2023-04-15 15:31:57 +0200

    added default value for image format

--- Core/Dtos/ScreenshotOptions.cs
+++ Core/Dtos/ScreenshotOptions.cs
@@ -1,6 +1,9 @@
-﻿namespace Core.Dtos;
+﻿using System.ComponentModel;
+
+namespace Core.Dtos;
 public class ScreenshotOptions
 {
+    [DefaultValue("jpeg")]
     public string? ImageFormat { get; set; }
 }
 
