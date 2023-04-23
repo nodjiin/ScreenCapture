071d532285c71fd7f014bd26eab64bed81ff91d3
alex casadio <alex.casadio92@gmail.com>
2023-04-23 19:41:58 +0200

    Implemented base RemoteAgentCard visualization

--- ScreenCapture.WebApp/Objects/RemoteAgent.cs
+++ ScreenCapture.WebApp/Objects/RemoteAgent.cs
@@ -10,21 +10,36 @@ public class RemoteAgent
 
     public string Ip { get; init; }
     public string Label { get; init; }
-    public RemoteAgentStatus Status { get; }
+    public RemoteAgentStatus Status { get; private set; }
 
-    public Task UpdateStatusAsync(HttpClient http)
+    public DateTime? LastOnline { get; private set; }
+
+    public Task UpdateStatusAsync()
     {
         throw new NotImplementedException();
     }
 
-    public Task StartRecordingAsync(HttpClient http) { throw new NotImplementedException(); }
+    public Task StartRecordingAsync()
+    {
+        Status = RemoteAgentStatus.Recording;
+        return Task.CompletedTask;
+    }
 
-    public Task StopRecordingAsync(HttpClient http) { throw new NotImplementedException(); }
+    public Task StopRecordingAsync()
+    {
+        Status = RemoteAgentStatus.Online;
+        return Task.CompletedTask;
+    }
+    public Task TakeScreenshotAsync()
+    {
+        Status = RemoteAgentStatus.Offline;
+        return Task.CompletedTask;
+    }
 }
 
 public enum RemoteAgentStatus
 {
-    Online,
     Offline,
+    Online,
     Recording
 }
