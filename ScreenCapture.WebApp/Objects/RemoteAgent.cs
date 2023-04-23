920c4c8f7b27adfddb4dad07415e2f8e78b194dd
alex casadio <alex.casadio92@gmail.com>
2023-04-23 17:55:54 +0200

    added first basic component for the RemoteAgents page

---
+++ ScreenCapture.WebApp/Objects/RemoteAgent.cs
@@ -0,0 +1,30 @@
+﻿namespace ScreenCapture.WebApp.Objects;
+
+public class RemoteAgent
+{
+    public RemoteAgent(string ip, string label)
+    {
+        Ip = ip;
+        Label = label;
+    }
+
+    public string Ip { get; init; }
+    public string Label { get; init; }
+    public RemoteAgentStatus Status { get; }
+
+    public Task UpdateStatusAsync(HttpClient http)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StartRecordingAsync(HttpClient http) { throw new NotImplementedException(); }
+
+    public Task StopRecordingAsync(HttpClient http) { throw new NotImplementedException(); }
+}
+
+public enum RemoteAgentStatus
+{
+    Online,
+    Offline,
+    Recording
+}
