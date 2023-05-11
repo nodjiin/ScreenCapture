2010e1c334e7aa6dc965e3be35ac556122ed57d3
alex casadio <alex.casadio92@gmail.com>
2023-05-11 16:16:58 +0200

    NotificationComponent: moved code in separate file

---
+++ ScreenCapture.WebApp/Shared/NotificationComponent.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using ScreenCapture.WebApp.Domain;
+
+namespace ScreenCapture.WebApp.Shared;
+
+public partial class NotificationComponent
+{
+    [Inject] public IJSRuntime? js { get; set; }
+    private IJSObjectReference? module;
+
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    private NotificationLevel level;
+    private string message = string.Empty;
+
+    public async Task RaiseNotification(string notificationMessage, NotificationLevel notificationLevel)
+    {
+        message = notificationMessage;
+        level = notificationLevel;
+        StateHasChanged();
+
+        if (js != null)
+        {
+            module ??= await js.InvokeAsync<IJSObjectReference>("import", "./Shared/NotificationComponent.razor.js");
+            await module.InvokeVoidAsync("showToast");
+        }
+    }
+
+    private string GetNotificationColor()
+    {
+        switch (level)
+        {
+            case NotificationLevel.Success:
+                return "bg-success";
+            case NotificationLevel.Warning:
+                return "bg-warning";
+            case NotificationLevel.Error:
+                return "bg-danger";
+            case NotificationLevel.Information:
+                return "bg-info";
+            default:
+                return "bg-secondary";
+        }
+    }
+}
